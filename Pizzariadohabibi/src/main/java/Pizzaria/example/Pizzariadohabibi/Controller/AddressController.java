2815ceb5356830db2373b1261337f55d0414688a
hasan essaad <hasanessaad@gmail.com>
2023-08-17 15:54:43 -0300

    3rd

--- Pizzariadohabibi/src/main/java/Pizzaria/example/Pizzariadohabibi/Controller/AddressController.java
+++ Pizzariadohabibi/src/main/java/Pizzaria/example/Pizzariadohabibi/Controller/AddressController.java
@@ -1,11 +1,42 @@
 package Pizzaria.example.Pizzariadohabibi.Controller;
 
 
+import Pizzaria.example.Pizzariadohabibi.DTO.AddressDto;
+import Pizzaria.example.Pizzariadohabibi.Entity.Address;
+import Pizzaria.example.Pizzariadohabibi.Repository.AddressRepository;
+import Pizzaria.example.Pizzariadohabibi.Service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/api/address")
 public class AddressController {
+    @Autowired
+    private AddressRepository addressRepository;
 
+    @Autowired
+    private AddressService addressService;
+
+    @GetMapping("/findAll")
+    public ResponseEntity<?> findAll(){
+
+        try{
+            List<?> addresses = addressRepository.findAll();
+            return new ResponseEntity<>(addresses, HttpStatus.OK);
+        }catch (Exception e){
+
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/{nameperson}")
+    public ResponseEntity<Address> createLembrete(@PathVariable String nameperson, @RequestBody AddressDto addressDto) {
+        Address address = addressService.cadastrar(nameperson,addressDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(address);
+    }
 }
